5154cfe69cc86f312ee8b532e5729aec17ff82b2
kevduc <kduconge@gmail.com>
2022-09-27 20:28:13 +0100

    Handle directory input

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import re
 import argparse
+import os
+import glob
 
 # Parse arguments (input and output file names)
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", type=str,
-                    help="Input file (list of HTML link tags)")
+                    help="Input file (file containing a list of HTML link tags) or directory (containing multiple .html files to convert)")
 parser.add_argument("-o", "--output", type=str,
                     help="Output file (generated XML Sitemap)")
 args = parser.parse_args()
@@ -17,33 +19,48 @@ defaultOutputFileName = "output.xml"
 inputFileName = args.input if args.input else defaultInputFileName
 outputFileName = args.output if args.output else defaultOutputFileName
 
-# Read input file
-inputFile = open(inputFileName, "r")
-text = inputFile.read()
+# If inputFileName is a directory, convert all the html files inside
+if os.path.isdir(inputFileName):
+    print("Input is a directory, ignoring output file name.")
+    inputFileNames = glob.glob(f'{inputFileName}/*.html')
+    extensionRegex = re.compile('\.html$', re.IGNORECASE)
+    outputFileNames = [extensionRegex.sub(
+        ".xml", fileName) for fileName in inputFileNames]
+else:
+    inputFileNames = [inputFileName]
+    outputFileNames = [outputFileName]
 
-# Extract all the html <link> tags and the required attributes
+# To extract all the html <link> tags and the required attributes
 regex = re.compile('\s*<link(?:(?:\s+rel="(?P<rel>.*?)")|(?:\s+hreflang="(?P<hreflang>.*?)")|(?:\s+href="(?P<href>.*?)")|(?:\s+.*?))*?\s*/>',
                    re.MULTILINE | re.IGNORECASE | re.DOTALL)
-links = [link.groupdict() for link in regex.finditer(text)]
 
-# Default url hreflang value
-defaultHreflang = "x-default"
+for i, inputFileName in enumerate(inputFileNames):
 
-# Generate XML output
-newline = "\n"
+    # Read input file
+    inputFile = open(inputFileName, "r")
+    text = inputFile.read()
 
-alternates = newline.join(
-    f'    <xhtml:link rel="{link["rel"]}" hreflang="{link["hreflang"]}" href="{link["href"]}"/>' for link in links)
+    # Extract all the html <link> tags and the required attributes
+    links = [link.groupdict() for link in regex.finditer(text)]
 
-output = f"""<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-{newline.join(f'''  <url>
-    <loc>{link["href"]}</loc>
-{alternates}
-  </url>''' for link in links if link["hreflang"] != defaultHreflang)}
-</urlset>
-"""
+    # Default url hreflang value
+    defaultHreflang = "x-default"
 
-# Write output file
-outputFile = open(outputFileName, "w")
-outputFile.write(output)
+    # Generate XML output
+    newline = "\n"
+
+    alternates = newline.join(
+        f'    <xhtml:link rel="{link["rel"]}" hreflang="{link["hreflang"]}" href="{link["href"]}"/>' for link in links)
+
+    output = f"""<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+  {newline.join(f'''  <url>
+      <loc>{link["href"]}</loc>
+  {alternates}
+    </url>''' for link in links if link["hreflang"] != defaultHreflang)}
+  </urlset>
+  """
+
+    # Write output file
+    outputFile = open(outputFileNames[i], "w")
+    outputFile.write(output)
